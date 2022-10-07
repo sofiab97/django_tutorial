bf7004a8b2e96dbb6b940f5080cce94710974a37
sofiab97 <sofibtzc@gmail.com>
2022-10-07 20:27:38 +0000

    Paso 2: Crear entidades

--- mysite/trazabilidad/models.py
+++ mysite/trazabilidad/models.py
@@ -1,3 +1,27 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+
+class Animal(models.Model):
+    referencia = models.CharField(max_length=200)
+    nacimiento = models.DateTimeField('Fecha de nacimiento')
+    genero = models.CharField(max_length=200)
+    produccion = models.BooleanField(default=False)
+    estado = models.CharField(max_length=200)
+    peso = models.CharField(max_length=200)
+    raza = models.CharField(max_length=200)
+    obs = models.CharField(max_length=200)
+
+
+
+class Propietario(models.Model):
+    propietario = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=200)
+    CI = models.CharField(max_length=200)
+    Establecimiento = models.CharField(max_length=200)
+    RUC = models.CharField(max_length=200)
+    departamento = models.CharField(max_length=200)
+    establecimiento = models.CharField(max_length=200)
+    ubicacion = models.CharField(max_length=200)
+    ciudad = models.CharField(max_length=200)
+    activo = models.BooleanField(default=False)
